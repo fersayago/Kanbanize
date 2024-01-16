b78c13b66e54a586b0060424a1e286404f58f5ba
fersayago <sayago.fernando@gmail.com>
2024-01-16 04:32:34 -0300

    feat: :sparkles: navigation bar and shadcn components

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
+
+export const OrgControl = () => {
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+
+  React.useEffect(() => {
+    if (!setActive) return;
+
+    setActive({
+      organization: params.organizationId as string,
+    });
+  }, [setActive, params.organizationId]);
+
+  return null;
+};
