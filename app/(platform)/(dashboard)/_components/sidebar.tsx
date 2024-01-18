7c56bd8b7c4dca4d990a0ba53762d70527d9a193
fersayago <sayago.fernando@gmail.com>
2024-01-18 10:40:31 -0300

    feat: :lipstick: Skeleton items for sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -50,7 +50,14 @@ const Sidebar = ({ storageKey = "k-sidebar-state" }: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]"/>
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
