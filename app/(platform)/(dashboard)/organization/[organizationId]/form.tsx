02eed1f72be8e83b262cd10ce34e6d9435fdc4e9
fersayago <sayago.fernando@gmail.com>
2024-01-22 19:27:30 -0300

    refactor: :recycle: Form components (errors, input, submit)

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { createBoard } from "@/actions/create-board";
-
-import FormInput from "./form-input";
-import FormButton from "./form-button";
 import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
+import FormInput from "@/components/form/form-input";
+import FormSubmit from "@/components/form/form-submit";
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -25,9 +24,9 @@ export const Form = () => {
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput errors={fieldErrors} id="title" label="Board Title"/>
       </div>
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
