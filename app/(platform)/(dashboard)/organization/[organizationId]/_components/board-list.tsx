7ba3ed1321e5326dd586ee954a5769b4f05f7bd3
fersayago <sayago.fernando@gmail.com>
2024-01-26 15:23:02 -0300

    feat: :sparkles: add board list
    implementation of boards view in the organization boards section. added suspense with skeleton for when loading

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,8 +1,29 @@
-import FormPopover from "@/components/form/form-popover";
-import Hint from "@/components/hint";
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import { HelpCircle, User2 } from "lucide-react";
 
-const BoardList = () => {
+import { db } from "@/lib/db";
+import Hint from "@/components/hint";
+import FormPopover from "@/components/form/form-popover";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const BoardList = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect("/select-org");
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +31,17 @@ const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            href={`/board/${board.id}`}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <div
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
@@ -25,6 +57,27 @@ const BoardList = () => {
             </Hint>
           </div>
         </FormPopover>
+      </div>
+    </div>
+  );
+};
+
+BoardList.Skeleton = function SKeletonBoardList() {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
+        <Skeleton className="aspect-video h-full w-full p-2" />
       </div>
     </div>
   );
