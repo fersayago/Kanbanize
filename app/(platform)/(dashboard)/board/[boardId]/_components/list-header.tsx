fe1aace5cf55331783ef280dfb1cb75bb0d187b4
fersayago <sayago.fernando@gmail.com>
2024-01-30 13:40:35 -0300

    feat: :sparkles: List options
    list options to copy and delete implemented with corresponding server actions.

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -8,6 +8,7 @@ import { useState, useRef, ElementRef } from "react";
 import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
 import FormInput from "@/components/form/form-input";
+import ListOptions from "./list-options";
 
 interface ListHeaderProps {
   data: List;
@@ -98,6 +99,10 @@ const ListHeader = ({ data }: ListHeaderProps) => {
           {title}
         </div>
       )}
+      <ListOptions
+        onAddCard={() => {}}
+        data={data}
+      />
     </div>
   );
 };
